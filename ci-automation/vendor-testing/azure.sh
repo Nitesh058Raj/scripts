a297f70b4151ea5936dc3128eb5cfe4b4104acb8
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2024-02-07 14:11:41 +0100

    tmp! mantle avset support
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@microsoft.com>

--- ci-automation/vendor-testing/azure.sh
+++ ci-automation/vendor-testing/azure.sh
@@ -59,6 +59,8 @@ run_kola_tests() {
       ${AZURE_USE_GALLERY} \
       ${azure_vnet_subnet_name:+--azure-vnet-subnet-name=${azure_vnet_subnet_name}} \
       ${AZURE_USE_PRIVATE_IPS:+--azure-use-private-ips=${AZURE_USE_PRIVATE_IPS}} \
+      ${AZURE_RESOURCE_GROUP:+--azure-resource-group=${AZURE_RESOURCE_GROUP}} \
+      ${AZURE_AVSET_ID:+--azure-availability-set-id=${AZURE_AVSET_ID}} \
       "${@}"
 }
 
@@ -69,7 +71,7 @@ query_kola_tests() {
 
 other_instance_types=()
 if [[ "${CIA_ARCH}" = 'amd64' ]]; then
-    other_instance_types+=('V1' 'Standard_NC6s_v3')
+    : #other_instance_types+=('V1' 'Standard_NC6s_v3')
 fi
 
 run_kola_tests_on_instances \
